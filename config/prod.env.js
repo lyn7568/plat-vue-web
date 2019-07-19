ef459e32f0ae356acfa547da45b5c5df72243164
lyn7568 <1271778060@qq.com>
2019-07-19 16:18:06 +0800

    添加 keywords describe

--- config/prod.env.js
+++ config/prod.env.js
@@ -4,8 +4,8 @@ module.exports = {
   ENV_CONFIG:'"prod"',
   BASE_API: '""',
   PLAT_ID: '"F5DEA8EDB12E421A9FC4B701BB6176A6"',
-  // KX_API: '"http://www.ekexiu.com"',
-  // HT_API: '"http://www.xttjpt.cn:88"'
-  KX_API: '"http://192.168.3.233:81"',
-  HT_API: '"http://192.168.3.233:83"',
+  KX_API: '"http://www.ekexiu.com"',
+  HT_API: '"http://www.xttjpt.cn:88"'
+  // KX_API: '"http://192.168.3.233:81"',
+  // HT_API: '"http://192.168.3.233:83"',
 }
